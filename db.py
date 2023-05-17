58a1633820aa155e7124d440ba28f2fc5206542e
ogabek <quvvatullayev@gmail.com>
2023-05-17 11:43:51 +0500

    create get user data and add user data

--- db.py
+++ db.py
@@ -5,6 +5,7 @@ class DB:
     def __init__(self, path) -> None:
         self.db = TinyDB(path, indent=4, separators=(',', ': '))
         self.users = self.db.table('users')
+        self.userdata = self.db.table('user_data')
 
     def add_user(self, chat_id, name, surname, phone, telegram, area, school, class_):
         if not self.users.get(doc_id=int(chat_id)):
@@ -33,3 +34,33 @@ class DB:
             return "200"
         else:
             return "401"
+        
+    def user_data(self, chat_id, name=None, surname=None, phone=None, telegram=None, area=None, school=None, class_=None):
+        chat_id = int(chat_id)
+        if self.userdata.get(chat_id = chat_id):
+            self.userdata.update(Document({
+                "chat_id": chat_id,
+                "name": name,
+                "surname": surname,
+                "phone": phone,
+                "username": telegram,
+                "area": area,
+                "school": school,
+                "class": class_
+                }, doc_id=chat_id))
+        else:
+            self.userdata.insert(Document({
+                "chat_id": chat_id,
+                "name": name,
+                "surname": surname,
+                "phone": phone,
+                "username": telegram,
+                "area": area,
+                "school": school,
+                "class": class_
+                }, doc_id=chat_id))
+            
+    def get_user_data(self, chat_id):
+        chat_id = int(chat_id)
+        return self.userdata.get(doc_id=chat_id)
+        
